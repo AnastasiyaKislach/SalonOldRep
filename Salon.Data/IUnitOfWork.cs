630dbffe10a705a49e549ebef5525cd439da5553
AnastasiyaKislach <anastasiya.kislach@gmail.com>
2019-12-26 19:00:29 +0300

    Data layer
    Added uow

---
+++ Salon.Data/IUnitOfWork.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Storage;
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+using Salon.Data.Repository;
+
+namespace Salon.Data
+{
+	public interface IUnitOfWork : IDisposable
+	{
+		void ChangeTrackingBehaviour(QueryTrackingBehavior trackingBehavior);
+
+		IRepository Repository { get; }
+
+		int SaveChanges(bool acceptAllChangesOnSuccess);
+
+		int SaveChanges();
+
+		Task<int> SaveChangesAsync(bool acceptAllChangesOnSuccess, CancellationToken cancellationToken = new CancellationToken());
+
+		Task<int> SaveChangesAsync(CancellationToken cancellationToken = new CancellationToken());
+
+		Task<IDbContextTransaction> CreateTransaction();
+	}
+}
---
+++ Salon.Data/UnitOfWork.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Storage;
+using System.Threading;
+using System.Threading.Tasks;
+using Salon.Data.Repository;
+
+namespace Salon.Data
+{
+	public class UnitOfWork : IUnitOfWork
+	{
+		private readonly DbContext _context;
+
+		public UnitOfWork(IRepository repository, DbContext context)
+		{
+			_context = context;
+			Repository = repository;
+		}
+
+		public async Task<IDbContextTransaction> CreateTransaction()
+		{
+			return await _context.Database.BeginTransactionAsync();
+		}
+
+		public void ChangeTrackingBehaviour(QueryTrackingBehavior trackingBehavior)
+		{
+			_context.ChangeTracker.QueryTrackingBehavior = trackingBehavior;
+		}
+
+		public IRepository Repository { get; private set; }
+
+
+		public int SaveChanges(bool acceptAllChangesOnSuccess)
+		{
+			return _context.SaveChanges(acceptAllChangesOnSuccess);
+		}
+
+		public int SaveChanges()
+		{
+			return _context.SaveChanges();
+		}
+
+		public async Task<int> SaveChangesAsync(bool acceptAllChangesOnSuccess, CancellationToken cancellationToken = default)
+		{
+			return await _context.SaveChangesAsync(acceptAllChangesOnSuccess, cancellationToken);
+		}
+
+		public async Task<int> SaveChangesAsync(CancellationToken cancellationToken = default)
+		{
+			return await _context.SaveChangesAsync(cancellationToken);
+		}
+
+		public void Dispose()
+		{
+			_context.Dispose();
+		}
+	}
+}
