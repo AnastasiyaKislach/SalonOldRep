328462b1f5b167237534600fae5af60b3bb3ec22
AnastasiyaKislach <anastasiya.kislach@gmail.com>
2019-12-26 19:00:19 +0300

    Data layer
    Added repository

---
+++ Salon.Data/Repository/IRepository.cs
@@ -0,0 +1,24 @@
+﻿using System.Linq;
+using System.Threading.Tasks;
+using Salon.Data.Entities;
+
+namespace Salon.Data.Repository
+{
+	public interface IRepository
+	{
+		IQueryable<TEntity> GetAll<TEntity>()
+			where TEntity : class, IEntityBase;
+
+		Task<TEntity> GetById<TEntity>(int id)
+			where TEntity : class, IEntityBase;
+
+		Task Create<TEntity>(TEntity entity)
+			where TEntity : class, IEntityBase;
+
+		Task Update<TEntity>(TEntity entity)
+			where TEntity : class, IEntityBase;
+
+		Task Delete<TEntity>(int id)
+			where TEntity : class, IEntityBase;
+	}
+}
---
+++ Salon.Data/Repository/Repository.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.Linq;
+using System.Threading.Tasks;
+using Salon.Data.Entities;
+
+namespace Salon.Data.Repository
+{
+    public class Repository : IRepository
+    {
+        private readonly DbContext _dbContext;
+
+        public Repository(DbContext context)
+        {
+            _dbContext = context;
+        }
+
+        public IQueryable<TEntity> GetAll<TEntity>()
+            where TEntity : class, IEntityBase
+        {
+            return _dbContext.Set<TEntity>().AsQueryable();
+        }
+
+        public async Task<TEntity> GetById<TEntity>(int id)
+            where TEntity : class, IEntityBase
+        {
+            return await _dbContext.Set<TEntity>().FindAsync(id);
+        }
+
+        public async Task Create<TEntity>(TEntity entity)
+            where TEntity : class, IEntityBase
+        {
+            var res = await _dbContext.Set<TEntity>().AddAsync(entity);
+        }
+
+        public Task Update<TEntity>(TEntity entity)
+            where TEntity : class, IEntityBase
+        {
+            _dbContext.Set<TEntity>().Update(entity);
+
+            return Task.CompletedTask;
+        }
+
+        public async Task Delete<TEntity>(int id)
+            where TEntity : class, IEntityBase
+        {
+            var entity = await GetById<TEntity>(id);
+            _dbContext.Set<TEntity>().Remove(entity);
+        }
+    }
+}
