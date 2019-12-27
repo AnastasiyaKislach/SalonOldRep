ea09d17d6747ee4fe3c8a4a902db31e043a70063
AnastasiyaKislach <anastasiya.kislach@gmail.com>
2019-12-27 17:19:13 +0300

    Service layer
    Added mapper

---
+++ Salon.Services/Automapper/MappingProfile.cs
@@ -0,0 +1,47 @@
+﻿using AutoMapper;
+using Salon.Data.Entities;
+using Salon.Services.Models;
+using System.Linq;
+
+namespace Salon.Services.Automapper
+{
+	public class MappingProfile : Profile
+	{
+		public MappingProfile()
+		{
+			CreateMap<AccessoryEntity, Accessory>();
+			CreateMap<Accessory, AccessoryEntity>();
+
+			CreateMap<AppointmentEntity, Appointment>()
+				.ForMember(a => a.Dresses, cfg => cfg.MapFrom(ae => ae.DressAppointments.Select(m => m.DressId)));
+			CreateMap<Appointment, AppointmentEntity>();
+
+			CreateMap<BrandEntity, Brand>()
+				.ForMember(a => a.Collections, cfg => cfg.MapFrom(ae => ae.Collections.Select(m => m.Id))); 
+			CreateMap<Brand, BrandEntity>();
+
+			CreateMap<CategoryEntity, Category>()
+				.ForMember(a => a.Accessories, cfg => cfg.MapFrom(ae => ae.Accessories.Select(m => m.Id)));
+			CreateMap<Category, CategoryEntity>();
+
+			CreateMap<CollectionEntity, Collection>()
+				.ForMember(a => a.Dresses, cfg => cfg.MapFrom(ae => ae.Dresses.Select(m => m.Id)));
+			CreateMap<Collection, CollectionEntity>();
+
+			CreateMap<Dress, DressEntity>();
+			CreateMap<DressEntity, Dress>();
+
+			CreateMap<News, NewsEntity>();
+			CreateMap<NewsEntity, News>();
+
+			CreateMap<Photo, PhotoEntity>();
+			CreateMap<PhotoEntity, Photo>();
+
+			CreateMap<Settings, SettingsEntity>();
+			CreateMap<SettingsEntity, Settings>();
+
+			CreateMap<Testimonial, TestimonialEntity>();
+			CreateMap<TestimonialEntity, Testimonial>();
+		}
+	}
+}
